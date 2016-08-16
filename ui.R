c7176ca715a23126b68359e61009658a5c7620bb
Stephan Koenig <stephan.koenig@gmail.com>
2016-08-15 17:26:28 -0700

    feat: Implement sidebar layout

--- ui.R
+++ ui.R
@@ -9,26 +9,29 @@ shinyUI(fluidPage(
   # Application title.
   titlePanel("The Shifting Meta"),
 
-  # Show a plot of the generated distribution
-  mainPanel(
-    # Display output$NAME defined in server.R.
-    plotOutput("meta_plot", width = "800px", height = "600px"),
+  # Choose layout with sidebar
+  sidebarLayout(
 
-    dataTableOutput("meta_table"),
-
-    # Slider input to select release.
-    fluidRow(
-      column(12, align="center",
-             sliderInput("release",
-                         "Release:",
-                         min = 1,
-                         max = length(releases),
-                         step = 1,
-                         value = length(releases),
-                         animate = TRUE)
-      )
+    sidebarPanel(
+      sliderInput("release",
+                  "Release:",
+                  min = 1,
+                  max = length(releases),
+                  step = 1,
+                  value = length(releases),
+                  animate = TRUE)
     ),
 
-    includeMarkdown("instructions.md")
+    mainPanel(
+
+      # Display output$NAME defined in server.R.
+      plotOutput("meta_plot"), #, width = "800px", height = "600px"
+
+      dataTableOutput("meta_table"),
+
+      # Slider input to select release.
+
+      includeMarkdown("instructions.md")
+    )
   )
 ))
